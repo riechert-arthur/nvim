afb903586d8be0c27cacaa82667fe57a65af8da8
agape <arthur@cybrco.ai>
2024-07-30 22:45:18 -0400

    added luasnip

--- lua/default_v1/core/keymaps.lua
+++ lua/default_v1/core/keymaps.lua
@@ -8,4 +8,16 @@ map("n", "<leader>fg", "<cmd>Telescope live_grep<cr>", options)
 map("n", "<leader>fb", "<cmd>Telescope buffers<cr>", options)
 map("n", "<leader>fh", "<cmd>Telescope help_tags<cr>", options)
 
+local luasnip = require("luasnip")
+map = vim.keymap.set
+
+map("i", "<C-k>", function() luasnip.expand() end, { silent = true })
+map({ "i", "s" }, "<C-l>", function() luasnip.jump(1) end, { silent = true })
+map({ "i", "s" }, "<C-j>", function() luasnip.jump(-1) end, { silent = true })
+map({ "i", "s" }, "<C-e>", function()
+	if luasnip.choice_active() then
+		luasnip.change_choice(1)
+	end
+end, { silent = true })
+
 return {}
