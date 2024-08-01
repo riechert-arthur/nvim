dc43c6f629feb20362f6014ab2826135f998242a
agape <arthur@cybrco.ai>
2024-08-01 02:40:41 -0400

    adjusted default auto tab options

--- lua/default_v1/core/options.lua
+++ lua/default_v1/core/options.lua
@@ -1,1 +1,10 @@
+vim.opt.number = true
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
+vim.opt.expandtab = true
+vim.opt.softtabstop = 2
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+vim.opt.clipboard:append("unnamedplus")
+
 return {}
